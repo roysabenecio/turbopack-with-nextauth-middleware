2d91c0b16f24f882b638824ed761d51c97172cc6
io-sono-roy <sabenecio.083905@laspinas.sti.edu.ph>
2023-06-23 20:34:05 +0800

    removed nav button to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
-
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return <Link href={"/dashboard"}>To Dashboard</Link>;
+  return null;
 }
